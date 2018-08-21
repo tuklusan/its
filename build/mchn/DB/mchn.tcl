e4e5d638d13e566e516c2803e6aa35df12fd142c
Lars Brinkhoff <lars@nocrew.org>
2018-08-21 02:16:52 -0700

    Eight RP06s.

--- build/mchn/DB/mchn.tcl
+++ build/mchn/DB/mchn.tcl
@@ -9,5 +9,12 @@ proc peek_switches {} {
 }
 
 proc mark_packs {} {
-    mark_pack "0" "0" "foobar"
+    mark_pack "0" "0" "LC#0"
+    mark_pack "1" "1" "LC#1"
+    mark_pack "2" "2" "LC#2"
+    mark_pack "3" "3" "LC#3"
+    mark_pack "4" "4" "LC#4"
+    mark_pack "5" "5" "LC#5"
+    mark_pack "6" "6" "LC#6"
+    mark_pack "7" "7" "LC#7"
 }
